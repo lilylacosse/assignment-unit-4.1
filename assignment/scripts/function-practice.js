1f1b07bdd5384d64c3e78a2f7f36e066330f7042
Lily LaCosse <lilylacosse@gmail.com>
2023-02-06 21:02:21 -0600

    Completed Stretch Goal #9

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -71,11 +71,15 @@ console.log('The last sport in the array is now:', getLast(sports));
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
-  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
 }
-
-
+console.log('Is tennis included in the array?',find('tennis', sports));
+console.log('Is fishing included in the array?',find('fishing', sports));
 
 // ----------------------
 // Stretch Goals
@@ -83,22 +87,31 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+ 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
   return sum;
 }
+
+let classVote = [5, 3, 3, 4, 5, 2, 1, 2];
+
+console.log('The sum of the class vote came to:', sumAll(classVote));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
+function allPositive(array) {
+  
+}
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
